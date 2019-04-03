cb8e42678ef7e445d8f19678d2b0738550f82aa8
hsernos <40272768+hsernos@users.noreply.github.com>
2019-04-03 22:52:17 +0800

    Add files via upload

---
+++ install/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# 安装curl
+info=`opkg list_installed | grep "curl"`
+if [ ${#info} == 0 ]; then
+	opkg install libpolarssl.ipk libcurl.ipk curl.ipk
+fi
+
+# 复制相关文件到对应位置
+chmod u+x feiy
+cp feiy /bin
+cp feiy.conf /etc/
+cat passdata.txt >> /etc/feiy.conf
+
+# 设置开机自启
+sed -i "/^feiy -login/ c\ " /etc/rc.local
+echo "feiy -login" >> /etc/rc.local
